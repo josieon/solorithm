8a34e8d6e5a82cffef460e4d1a2be7693e3d0f36
josieon <76546008+josieon@users.noreply.github.com>
2022-10-20 14:40:35 +0900

    [level 0] Title: 로그인 성공?, Time: 0.16 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        for(int i = 0; i < db.length; i++){
+            if(db[i][0].matches(id_pw[0])){
+                if(db[i][1].matches(id_pw[1]))
+                    return "login";
+                else
+                    return "wrong pw";
+            }
+        }
+        return "fail";
+    }
+}
