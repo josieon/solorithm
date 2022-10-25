2a87a38d77f8f995e70b83cd29750b1d17df7c57
Josieon <76546008+josieon@users.noreply.github.com>
2022-10-26 05:03:48 +0900

    [level 2] Title: 타겟 넘버, Time: 3.46 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,42 @@
+import java.util.*;
+class node{
+    public int value;
+    public int level;
+    node(int value, int level){
+        this.value = value;
+        this.level = level;
+    }
+}
 class Solution {
     static int count = 0;
-    public int solution(int[] numbers, int target) {
-        dfs(0, 0, numbers, target);
+    public int solution(int[] numbers, int target){
+        Queue<node> q = new LinkedList();
+        q.add(new node(0, 0));
+        while(!q.isEmpty()){
+            node tmp = q.poll();
+            if(tmp.level != numbers.length){
+                q.add(new node(tmp.value + numbers[tmp.level], tmp.level+1));
+                q.add(new node(tmp.value - numbers[tmp.level], tmp.level+1));
+            }
+            else{
+                if(tmp.value == target)
+                    count++;
+            }
+        }
         return count;
     }
-    public static void dfs(int idx, int sum, int[] numbers, int target){
-        if(idx == numbers.length){
-            if(sum == target)
-                count++;
-            return;
-        }
-        dfs(idx+1, sum + numbers[idx], numbers, target);
-        dfs(idx+1, sum - numbers[idx], numbers, target);
-    }
+    // DFS
+    // public int solution(int[] numbers, int target) {
+    //     dfs(0, 0, numbers, target);
+    //     return count;
+    // }
+    // public static void dfs(int idx, int sum, int[] numbers, int target){
+    //     if(idx == numbers.length){
+    //         if(sum == target)
+    //             count++;
+    //         return;
+    //     }
+    //     dfs(idx+1, sum + numbers[idx], numbers, target);
+    //     dfs(idx+1, sum - numbers[idx], numbers, target);
+    // }
 }
