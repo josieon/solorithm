9466b52b4254a8d10050078081b168e625af2710
josieon <76546008+josieon@users.noreply.github.com>
2022-10-26 01:07:18 +0900

    [level 0] Title: 등수 매기기, Time: 0.46 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -1,45 +1,52 @@
 import java.util.*;
 class Solution {
     //수정 후.
-    public int[] solution(int[][] score){
-        int[] answer = new int[score.length];
-        float[] avg = new float[score.length];
-        for(int i = 0; i < score.length; i++)
-            avg[i] = (float)(score[i][0] + score[i][1])/2;
-        for(int i = 0; i < score.length; i++){
-            answer[i] = 1;
-            for(int j = 0; j < avg.length; j++){
-                if(avg[i] < avg[j])
-                    answer[i]++;
-            }
-        }
-        return answer;
-    }
-    // public int[] solution(int[][] score) {
+    // public int[] solution(int[][] score){
+    //     int[] answer = new int[score.length];
     //     float[] avg = new float[score.length];
-    //     for(int i = 0; i < avg.length; i++)
-    //         avg[i] = (float)(score[i][0] + score[i][1]) / 2;
-    //     float[] rank = avg.clone();
-    //     int[] answer = new int[rank.length];
-    //     Arrays.sort(avg);
-    //     int r = 1;
-    //     for(int i = avg.length-1; i >= 0; i--){
-    //         int cnt = 1;
-    //         float val = avg[i];
-    //         while(i > 0 && avg[i] == avg[i-1]){
-    //             cnt++;
-    //             i--;
+    //     for(int i = 0; i < score.length; i++)
+    //         avg[i] = (float)(score[i][0] + score[i][1])/2;
+    //     for(int i = 0; i < score.length; i++){
+    //         answer[i] = 1;
+    //         for(int j = 0; j < avg.length; j++){
+    //             if(avg[i] < avg[j])
+    //                 answer[i]++;
     //         }
-    //         int tmp = cnt;
-    //         for(int j = 0; cnt > 0; j++){
-    //             if(val == rank[j]){
-    //                 answer[j] = r;
-    //                 cnt--;
-    //             }
-    //         }
-    //         r += tmp;
-    //         // i -= tmp;
     //     }
     //     return answer;
     // }
+        public static int[] solution(int[][] score){
+        int[] answer = new int[score.length];
+        float[] avg = new float[score.length];
+        for(int i = 0; i < score.length; i++)
+            avg[i] = (float)(score[i][0] + score[i][1]) / 2;
+        PriorityQueue<Float> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(float f : avg)
+            pq.add(f);
+        // StringBuilder sb = new StringBuilder();
+        ArrayList<Float> arr = new ArrayList<>();
+        HashMap<Float, Integer> hash = new HashMap<>();
+        int target = 1, cnt = 1, idx = 0;
+        while(!pq.isEmpty()){
+            float k = pq.poll();
+            if(arr.isEmpty()){
+                arr.add(k);
+                hash.put(k, target);
+            }
+            else{
+                if(arr.get(idx) > k){
+                    arr.add(k);
+                    target += cnt;
+                    cnt = 1;
+                    idx++;
+                    hash.put(k, target);
+                }
+                else
+                    cnt += 1;
+            }
+        }
+        for(int i = 0; i < answer.length; i++)
+            answer[i] = hash.get(avg[i]);
+        return answer;
+    }
 }
