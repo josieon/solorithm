09d981a601244a3901b0e266716fc873ec0c9e53
Josieon <76546008+josieon@users.noreply.github.com>
2022-11-02 21:24:22 +0900

    [level 0] Title: 다항식 더하기, Time: 0.14 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,70 +1,92 @@
 import java.util.*;
-class Solution {
-    public String solution(String polynomial) {
-        int xs = 0, normal = 0;
-        String [] polynomials = polynomial.split(" ");
-        for(String polynom : polynomials){
-            if(polynom.equals("+")){
-                continue;
-            }
-            if(!polynom.contains("x")){
-                normal += Integer.parseInt(polynom);
-            }else{
-                if(polynom.length() == 1){
-                    xs += 1;
-                }else{
-                    xs += Integer.parseInt(polynom.substring(0, polynom.length()-1));
-                }
-            }
-        }
-        if(xs == 0 && normal == 0){
-            return "0";
-        }else if(xs != 0 && normal == 0){
-            if(xs == 1){
-                return "x";
-            }else{
-                return String.valueOf(xs) + "x";
-            }
-        }else if(xs == 0 && normal != 0){
-            return String.valueOf(normal);
-        }else{
-            if(xs == 1){
-                return "x + " + String.valueOf(normal);
-            }else{
-                return String.valueOf(xs) + "x + " + String.valueOf(normal); 
-
-            }
-        }
-    }
-}
 // class Solution {
 //     public String solution(String polynomial) {
-//         String[] arr = polynomial.replaceAll(" ", "").split("\\+");
-//         if(arr.length == 1)
-//             return arr[0];
-//         int x = 0, a = 0;
-//         for(String tmp : arr){
-//             // String tmp = t.trim();
-//             if(tmp.length() > 1)
-//                 x += Integer.parseInt(tmp.substring(0, tmp.length() - 1));
-//             else{
-//                 if(tmp.contains("x"))
-//                     x++;
-//                 else
-//                     a += Integer.parseInt(tmp);
+//         int xs = 0, normal = 0;
+//         String [] polynomials = polynomial.split(" ");
+//         for(String polynom : polynomials){
+//             if(polynom.equals("+")){
+//                 continue;
+//             }
+//             if(!polynom.contains("x")){
+//                 normal += Integer.parseInt(polynom);
+//             }else{
+//                 if(polynom.length() == 1){
+//                     xs += 1;
+//                 }else{
+//                     xs += Integer.parseInt(polynom.substring(0, polynom.length()-1));
+//                 }
 //             }
 //         }
-//         StringBuilder sb = new StringBuilder();
-//         if(x > 0){
-//             if(x > 1)
-//                 sb.append(x);
-//             sb.append("x");
+//         if(xs == 0 && normal == 0){
+//             return "0";
+//         }else if(xs != 0 && normal == 0){
+//             if(xs == 1){
+//                 return "x";
+//             }else{
+//                 return String.valueOf(xs) + "x";
+//             }
+//         }else if(xs == 0 && normal != 0){
+//             return String.valueOf(normal);
+//         }else{
+//             if(xs == 1){
+//                 return "x + " + String.valueOf(normal);
+//             }else{
+//                 return String.valueOf(xs) + "x + " + String.valueOf(normal); 
+
+//             }
 //         }
-//         if(a > 0){
-//             if(x > 0)
-//                 sb.append(" + ");
-//             sb.append(a);
-//         }
-//         return sb.toString();
 //     }
 // }
+class Solution {
+    public String solution(String polynomial) {
+        String[] arr = polynomial.replaceAll(" ", "").split("\\+");
+        if(arr.length == 1)
+            return arr[0];
+        int x = 0, a = 0;
+        for(String tmp : arr){
+            // String tmp = t.trim();
+            //-----최종 수정한 부분--------
+            if(!tmp.contains("x"))
+                a += Integer.parseInt(tmp);
+            else{
+                if(tmp.length() == 1)
+                    x += 1;
+                else
+                    x += Integer.parseInt(tmp.substring(0, tmp.length() - 1));
+            }
+            //-----최종 수정한 부분--------
+            // if(tmp.length() > 1)
+            //     x += Integer.parseInt(tmp.substring(0, tmp.length() - 1));
+            // else{
+            //     if(tmp.contains("x"))
+            //         x++;
+            //     else
+            //         a += Integer.parseInt(tmp);
+            // }
+        }
+        StringBuilder sb = new StringBuilder();
+        if(x == 0 && a == 0)
+            return "0";
+        if(x > 0){
+            if(x > 1)
+                sb.append(x);
+            sb.append("x");
+        }
+        if(a > 0){
+            if(x > 0)
+                sb.append(" + ");
+            sb.append(a);
+        }
+        // if(x > 0){
+        //     if(x > 1)
+        //         sb.append(x);
+        //     sb.append("x");
+        // }
+        // if(a > 0){
+        //     if(x > 0)
+        //         sb.append(" + ");
+        //     sb.append(a);
+        // }
+        return sb.toString();
+    }
+}
