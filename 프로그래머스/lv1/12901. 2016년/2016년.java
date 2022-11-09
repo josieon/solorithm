9e323b5880181ce09d1c5e387ad864bf051bf344
Josieon <76546008+josieon@users.noreply.github.com>
2022-11-09 17:38:17 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int a, int b) {
+        int[] days = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] d = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int day = 0;
+        for(int i = 0; i < a-1; i++)
+            day += days[i];
+        day += (b-1);
+        return d[day%7];
+    }
+}
